85580a25a56e6c4901e6d3e34a4bf4efa5d5e1e0
Axenmagnus <Axenmagnus@users.noreply.github.com>
2022-06-23 15:26:15 +0200

    Implemented ArticleFetch

--- WebbsScrape.py
+++ WebbsScrape.py
@@ -20,9 +20,9 @@ from datetime import date
 from DateChanger import DateChanger,CompanyChange
 url="https://www.google.com/search?q=ericsson&biw=1440&bih=719&sxsrf=ALiCzsZxadn_z8_8rEjksk-Vq4mgzvngFw%3A1655816720043&source=lnt&tbs=cdr%3A1%2Ccd_min%3A9%2F15%2F2021%2Ccd_max%3A9%2F15%2F2021&tbm=nws"
 
-drag=date(2021, 9, 15)#Instantiation
-push=date(2021, 9, 16)#Instantiation
-
+drag=date(2020, 9, 15)#Instantiation #When to start article fetch
+push=date(2020, 9, 16)#Instantiation
+enddate=date.today()
 userAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 
 
@@ -38,49 +38,56 @@ headers={'User-Agent':userAgent,}
 
 
 # import requests
+ #Intended for easy gathering of articles for company for various dates.   
 
 # #url = 'https://www.google.com/'
 # r = requests.get(url)
 # print(r.text)
-days=100 # Set arbitrarily
-for i in(range(days)):
-    request=urllib.request.Request(url,None,headers) #The assembled request
-    response = urllib.request.urlopen(request)
-    page_html=response.read()
-    response.close()
-    page_soup=soup(page_html,"html.parser")
-    containers = page_soup.findAll("div", {"class": "xuvV6b BGxR7d"})
-    
-    for i in range(5):
-        article=containers[i]
-        container=containers[0]
-        #print(container==article)
-        #c=article.a
-    
-        contianer=container.a
-        #print(container==article)
-        #print(contianer)
-        for a in article.find_all('a', href=True):
-            print (a['href'])
-            temp_url=(a['href'])
-            #Here we may attain the articles.
-            # download and parse article
-            article = Article(temp_url)
-            article.download()
-            article.parse()
-             
-            # print article text
-            #print(article.text)
-    
+
+def ArticleFetch(company,drag,push,url,endDate):
+    url=CompanyChange(company,url)
     drag,push,url=DateChanger(drag,push,url)
     
+    while drag!=endDate:
+        request=urllib.request.Request(url,None,headers) #The assembled request
+        response = urllib.request.urlopen(request)
+        page_html=response.read()
+        response.close()
+        page_soup=soup(page_html,"html.parser")
+        containers = page_soup.findAll("div", {"class": "xuvV6b BGxR7d"})
+        
+        for i in range(5):
+            article=containers[i]
+            container=containers[0]
+            #print(container==article)
+            #c=article.a
+        
+            contianer=container.a
+            #print(container==article)
+            #print(contianer)
+            for a in article.find_all('a', href=True):
+                print (a['href'])
+                temp_url=(a['href'])
+                #Here we may attain the articles.
+                # download and parse article
+                article = Article(temp_url)
+                article.download()
+                article.parse()
+                 
+                # print article text
+                #print(article.text)
+        
+        drag,push,url=DateChanger(drag,push,url)
+        
     
     
+    return company
+
+
+
+
     
     
-    
- #Intended for easy gathering of articles for company for various dates.   
-#def ArticleFetch(company,start,end):
     
 
 
