35085700b7da77290a006553d505a69d175a7edc
Axenmagnus <Axenmagnus@users.noreply.github.com>
2022-06-30 22:00:01 +0200

    Implement timesleep

--- WebbsScrape.py
+++ WebbsScrape.py
@@ -11,7 +11,7 @@ Created on Tue Jun 14 20:42:56 2022
 Created on Tue Aug 17 17:28:23 2021
 @author: magnusaxen
 """
-
+import time
 # importing the library
 import urllib.request
 from bs4 import BeautifulSoup as soup
@@ -19,7 +19,7 @@ from newspaper import Article
 from datetime import date
 from DateChanger import DateChanger,CompanyChange
 from ValidityCheck import ValidityCheck
-url="https://www.google.com/search?q=ericsson&biw=1440&bih=719&sxsrf=ALiCzsZxadn_z8_8rEjksk-Vq4mgzvngFw%3A1655816720043&source=lnt&tbs=cdr%3A1%2Ccd_min%3A9%2F15%2F2021%2Ccd_max%3A9%2F15%2F2021&tbm=nws"
+url="https://www.google.com/search?q=ericsson-company&tbas=0&biw=1440&bih=719&sxsrf=ALiCzsYFi00-AP_3fTWwvftpsmeSDcubqg%3A1656605297794&source=lnt&tbs=sbd%3A1%2Ccdr%3A1%2Ccd_min%3A12%2F9%2F2021%2Ccd_max%3A12%2F9%2F2021&tbm=nws"
 
 drag=date(2020, 9, 15)#Instantiation #When to start article fetch
 push=date(2020, 9, 16)#Instantiation
@@ -47,56 +47,70 @@ headers={'User-Agent':userAgent,}
 # print(r.text)
 
 def ArticleFetch(company,drag,push,url,endDate):
+    counter=0
     url=CompanyChange(company,url)
     drag,push,url=DateChanger(drag,push,url)
     
     while drag!=endDate:
-        request=urllib.request.Request(url,None,headers) #The assembled request
-        response = urllib.request.urlopen(request)
-        page_html=response.read()
-        response.close()
-        page_soup=soup(page_html,"html.parser")
-        containers = page_soup.findAll("div", {"class": "xuvV6b BGxR7d"})
-        i=0
-        j=0
-        
-        while i<4:
-            print(i)
-            print(drag)
-            article=containers[i+j]
-            container=containers[0]
-            #print(container==article)
-            #c=article.a
-        
-            contianer=container.a
-            #print(container==article)
-            #print(contianer)
-            for a in article.find_all('a', href=True):
-                print (a['href'])
-                temp_url=(a['href'])
-                #Here we may attain the articles.
-                # download and parse article
-                article = Article(temp_url)
-                
-                article.download()
+        try:
+            request=urllib.request.Request(url,None,headers) #The assembled request
+            response = urllib.request.urlopen(request)
+            page_html=response.read()
+            response.close()
+            page_soup=soup(page_html,"html.parser")
+            containers = page_soup.findAll("div", {"class": "xuvV6b BGxR7d"})
+            i=0
+            j=0
+            val=4
+            if len(containers)<val:
+                val=len(containers)
+            while i<val:
+                print(i)
+                print(drag)
                 try:
-                    article.parse()
-                    i=i+1
-                    if ValidityCheck(company,article.text,page_html)==False:
-                        i=i-1
-                        j=j+1
+                    article=containers[i+j]
+                    container=containers[0]
+                except:
+                        i=i+1
+                        print(i)
+                #print(container==article)
+                #c=article.a
+            
+                contianer=container.a
+                #print(container==article)
+                #print(contianer)
+                try:
+                    for a in article.find_all('a', href=True):
+                        print (a['href'])
+                        temp_url=(a['href'])
+                        #Here we may attain the articles.
+                        # download and parse article
+                        article = Article(temp_url)
                         
+                        article.download()
+                        try:
+                            article.parse()
+                            i=i+1
+                            counter=counter+1
+                            print(counter)
+                            # if ValidityCheck(company,article.text,page_html)==False:
+                            #     i=i-1
+                            #     j=j+1
+                                
+                        except:
+                            j=j+1
                 except:
                     j=j+1
-    
-                
-                print(article.text)
-                #print(len(article.text))
-            #i+=1
         
-        
-        drag,push,url=DateChanger(drag,push,url)
-        
+                    
+                    #print(article.text)
+                    #print(len(article.text))
+                #i+=1
+            
+            
+            drag,push,url=DateChanger(drag,push,url)
+        except:
+            time.sleep(1)
     
     
     return company
